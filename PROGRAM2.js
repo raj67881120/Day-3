34628a5bea6e425f90af4a982f92c052bcc4c5d4
raj67881120 <110183502+raj67881120@users.noreply.github.com>
2022-08-09 09:53:31 +0530

    Add files via upload

---
+++ PROGRAM2.js
@@ -0,0 +1,20 @@
+function alertEX(){
+    alert("AND ARE YOU VACINATED");
+}
+function confirmEX(){
+    {
+        if(confirm("YOUR PRODUCTS ARE AVILABLE IN THIS SHOP!")){
+            alert("YES");
+        }
+        else{
+            alert("NO");
+            alert("SORRY FOR THIS SIR..")
+        }
+    }
+    
+}
+function PromptEX(){
+    var Name=prompt("ENTER YOUR NAME");
+    var BName=prompt("ENTER YOUR BRAND OF PRODUCTS");
+    alert(Name+" "+"YOU ARE BUYING"+" "+BName+" "+"PRODUCTS");
+}
